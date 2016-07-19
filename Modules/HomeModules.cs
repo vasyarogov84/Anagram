8ca9596910cdbfdafd03ef2247e593180982ba4e
Russ Davies and Steven Tran <russ+steven@>
2016-07-19 15:24:23 -0700

    add nancy pages to generate anagrams

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -6,12 +6,36 @@ namespace Anagram
 {
   public class HomeModule : NancyModule
   {
-    public HomModule() {
-      Get["/"] = _ => {
-        return View["/index.cshtml"];
+    public HomeModule()
+    {
+      Get["/"] = _ =>
+      {
+        return View["index.cshtml"];
+      };
 
-        
-      }
+      Post["/anagram_generated"] = _ =>
+      {
+        List<string> inputWords = new List<string>() {Request.Form["anagram1"], Request.Form["anagram2"], Request.Form["anagram3"], Request.Form["anagram4"], Request.Form["anagram5"],};
+        Anagram newAnagram = new Anagram() {};
+        List<string> results = new List<string>() {};
+        results = newAnagram.IsAnagramMultipleWords(inputWords);
+        return View["anagram_generated.cshtml", results];
+      };
+
+      Post["/partial_anagram_generated"] = _ =>
+      {
+        string results;
+        Anagram newAnagram = new Anagram() {};
+        if (newAnagram.IsAnagramPartial(Request.Form["partial1"], Request.Form["partial2"]))
+        {
+          results = "true";
+        }
+        else
+        {
+          results = "false";
+        }
+        return View["/partial_anagram_generated.cshtml", results];
+      };
     }
   }
 }
